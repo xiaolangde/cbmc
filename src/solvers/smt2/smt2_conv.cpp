b1d3d55d96ee21ea3600b4f9b869f5f8e9a6d192
Daniel Kroening <dkr@amazon.com>
2021-06-28 15:01:28 +0100

    remove encoding of arrays of bools into bitvector
    The encoding of arrays of booleans as arrays of (_ bitvec 1) was necessary
    when solvers did not support arrays of booleans.
    
    This support is now broadly available, removing the need for this code path.

--- src/solvers/smt2/smt2_conv.cpp
+++ src/solvers/smt2/smt2_conv.cpp
@@ -57,7 +57,6 @@ smt2_convt::smt2_convt(
   solvert _solver,
   std::ostream &_out)
   : use_FPA_theory(false),
-    use_array_of_bool(false),
     use_as_const(false),
     use_check_sat_assuming(false),
     use_datatypes(false),
@@ -86,7 +85,6 @@ smt2_convt::smt2_convt(
 
   case solvert::CPROVER_SMT2:
     use_FPA_theory = true;
-    use_array_of_bool = true;
     use_as_const = true;
     use_check_sat_assuming = true;
     emit_set_logic = false;
@@ -106,7 +104,6 @@ smt2_convt::smt2_convt(
     break;
 
   case solvert::Z3:
-    use_array_of_bool = true;
     use_as_const = true;
     use_check_sat_assuming = true;
     use_lambda_for_array = true;
@@ -3895,24 +3892,11 @@ void smt2_convt::convert_index(const index_exprt &expr)
 
     if(use_array_theory(expr.array()))
     {
-      if(expr.type().id() == ID_bool && !use_array_of_bool)
-      {
-        out << "(= ";
-        out << "(select ";
-        convert_expr(expr.array());
-        out << " ";
-        convert_expr(typecast_exprt(expr.index(), array_type.size().type()));
-        out << ")";
-        out << " #b1)";
-      }
-      else
-      {
-        out << "(select ";
-        convert_expr(expr.array());
-        out << " ";
-        convert_expr(typecast_exprt(expr.index(), array_type.size().type()));
-        out << ")";
-      }
+      out << "(select ";
+      convert_expr(expr.array());
+      out << " ";
+      convert_expr(typecast_exprt(expr.index(), array_type.size().type()));
+      out << ")";
     }
     else
     {
@@ -4455,16 +4439,7 @@ void smt2_convt::find_symbols(const exprt &expr)
         out << "(assert (forall ((i ";
         convert_type(array_type.size().type());
         out << ")) (= (select " << id << " i) ";
-        if(array_type.subtype().id() == ID_bool && !use_array_of_bool)
-        {
-          out << "(ite ";
-          convert_expr(array_of.what());
-          out << " #b1 #b0)";
-        }
-        else
-        {
-          convert_expr(array_of.what());
-        }
+        convert_expr(array_of.what());
         out << ")))\n";
 
         defined_expressions[expr] = id;
@@ -4503,16 +4478,7 @@ void smt2_convt::find_symbols(const exprt &expr)
         out << ")) (= (select " << id << " ";
         convert_expr(array_comprehension.arg());
         out << ") ";
-        if(array_type.subtype().id() == ID_bool && !use_array_of_bool)
-        {
-          out << "(ite ";
-          convert_expr(array_comprehension.body());
-          out << " #b1 #b0)";
-        }
-        else
-        {
-          convert_expr(array_comprehension.body());
-        }
+        convert_expr(array_comprehension.body());
         out << "))))\n";
 
         defined_expressions[expr] = id;
@@ -4536,16 +4502,7 @@ void smt2_convt::find_symbols(const exprt &expr)
         out << "(assert (= (select " << id << " ";
         convert_expr(from_integer(i, array_type.size().type()));
         out << ") "; // select
-        if(array_type.subtype().id() == ID_bool && !use_array_of_bool)
-        {
-          out << "(ite ";
-          convert_expr(expr.operands()[i]);
-          out << " #b1 #b0)";
-        }
-        else
-        {
-          convert_expr(expr.operands()[i]);
-        }
+        convert_expr(expr.operands()[i]);
         out << "))" << "\n"; // =, assert
       }
 
@@ -4690,16 +4647,11 @@ void smt2_convt::convert_type(const typet &type)
     CHECK_RETURN(array_type.size().is_not_nil());
 
     // we always use array theory for top-level arrays
-    const typet &subtype = array_type.subtype();
-
     out << "(Array ";
     convert_type(array_type.size().type());
     out << " ";
 
-    if(subtype.id()==ID_bool && !use_array_of_bool)
-      out << "(_ BitVec 1)";
-    else
-      convert_type(array_type.subtype());
+    convert_type(array_type.subtype());
 
     out << ")";
   }
